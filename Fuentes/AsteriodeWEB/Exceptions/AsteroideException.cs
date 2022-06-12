c94d56519cd498175beb9393dacf623c554a5301
anabelvelazkz <107347403+anabelvelazkz@users.noreply.github.com>
2022-06-12 12:39:12 +0200

    Add files via upload

---
+++ Fuentes/AsteriodeWEB/Exceptions/AsteroideException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsteriodeWEB.Exceptions
+{
+    public class AsteroideException : System.Exception
+    {
+        public AsteroideException(String msg) : base(msg)
+        {
+
+        }
+    }
+}
+
